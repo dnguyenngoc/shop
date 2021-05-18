3e5efa04ef4622d6dbf7e78a79a83afaf55ae590
Duy <duynguyenngoc@hotmail.com>
2021-05-19 00:09:03 +0700

    add some component

---
+++ frontend/src/components/logo/logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './logo.scss';
+
+const Logo = () => {
+  return (
+    <div className={styles.container}>
+      <span />
+      <span />
+      <span />
+    </div>
+  );
+};
+
+export default Logo;
